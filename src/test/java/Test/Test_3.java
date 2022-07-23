2cd2a714f66d1ab26ecef3ed4e9dda751b55c35d
GovindPatil75 <mulegr75@gmail.com>
2022-07-23 20:01:08 +0530

    Modifier Test 3

--- src/test/java/Test/Test_3.java
+++ src/test/java/Test/Test_3.java
@@ -5,6 +5,8 @@ public class Test_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Test 3");
+       
+       System.out.println("Added Some check Point");
 	}
 
 }
